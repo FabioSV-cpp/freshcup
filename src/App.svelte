7d52beb96a0ecad96191eb063e0fe3a9db87296d
Fabio Sousa Vieira <fabsousa23.fs@gmail.com>
2023-01-21 22:08:22 +0200

    Updated logo

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,14 @@
   import CoffeeBar from './lib/sub-menus/CoffeeBar.svelte';
   import Drinks from './lib/sub-menus/Drinks.svelte';
   import FoodMenu from './lib/sub-menus/FoodMenu.svelte';
-  import svelteLogo from './assets/svelte.svg';
+
+  import freshCup from './assets/freshcup.png';
 </script>
 
 <main
   class="bg-slate-100 flex flex-col justify-center items-center min-h-screen "
 >
-  <img src={svelteLogo} alt="svelte" class="h-36 mt-4" />
+  <img src={freshCup} alt="svelte" class="h-40 mt-4" />
 
   <article
     class="flex flex-col items-center justify-center py-4 px-2 text-black max-w-lg w-full flex-grow-[0.4]"
